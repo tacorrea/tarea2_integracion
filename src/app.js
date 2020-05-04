5e2a05effe8db4fcd1032d4ef21dcadd00f63869
Pablo Correa <pablocorrea@MacBook-Pro-de-Pablo.local>
2020-05-04 02:06:04 -0400

    eliminado app.js

--- src/app.js
+++ src/app.js
@@ -6,22 +6,6 @@ import projectRoutes from './routes/hamburguesa';
 import taskRoutes from './routes/ingrediente';
 //intialization
 const app = express();
-const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-
-client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
 
 
 //middlewares
