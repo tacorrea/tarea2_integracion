1c331c023adda993000f4607c6dc5978d0c1dac3
Pablo Correa <pablocorrea@MacBook-Pro-de-Pablo.local>
2020-05-04 00:20:11 -0400

    changed config

--- src/models/index.js
+++ src/models/index.js
@@ -6,30 +6,27 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 
-/*
 let config;
 if (env==="development") {
   config = require(__dirname + '/../config/config.json')[env]
 }
-*/
 const db = {};
 
 let sequelize;
 
 if (env==="production") {
   sequelize = new Sequelize(process.env["DATABASE_URL"], {});
-} 
-/*else {
+} else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
-*/
+
 fs
-  //.readdirSync(__dirname)
+  .readdirSync(__dirname)
   .filter(file => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    //const model = sequelize['import'](path.join(__dirname, file));
+    const model = sequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
   });
 
