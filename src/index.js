65fa3ce7c36ceb2f41e490a30ff9fbd9d2ca4b42
Pablo Correa <pablocorrea@MacBook-Pro-de-Pablo.local>
2020-05-04 15:17:24 -0400

    cambio env

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import app from './';
 import '@babel/polyfill';
 import regeneratorRuntime from "regenerator-runtime";
 
+
 async function main(){
     await app.listen(5432);
     console.log("server");
